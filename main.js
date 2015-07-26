5d15d344ffafe9c36c5122b02fa18f1f56b7c045
Yoshua Wuyts <i@yoshuawuyts.com>
2015-07-26 12:34:09 +1000

    {virtual-dom,main-loop} -> vel

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var h = require('vel/h')
-var main = require('main-loop')
-var loop = main({ n: 0 }, render, require('vel'))
-document.querySelector('#content').appendChild(loop.target)
+const vel = require('vel')
 
-function render (state) {
+const el = vel(function (h, state) {
   return h('div', [
     h('h1', 'clicked ' + state.n + ' times'),
     h('button', { onclick: onclick }, 'click me!')
   ])
+
   function onclick () {
-    loop.update({ n: state.n + 1 })
+    el({ n: state.n + 1 })
   }
-}
+})
+
+document.querySelector('#content').appendChild(el({ n: 0 }))
